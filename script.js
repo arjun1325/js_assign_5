413194415853fcd3415776bbec0e6a78cba0dac9
Arjun M <arjunajuoff@gmail.com>
2024-10-23 21:52:58 +0530

    html 5 and  fetch link added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+            let productsEl=""
+
+
+
+            for(let product of products){
+                productEl
+            }
