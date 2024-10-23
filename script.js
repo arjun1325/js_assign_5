71eca247a8d8977c2e8bf4034829b9df15c89e58
Arjun M <arjunajuoff@gmail.com>
2024-10-24 00:00:47 +0530

    card added with for loop

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
 fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+    .then(res => res.json())
+    .then(json => {
+        console.log(json)
 
-            let productsEl=""
+        let productEl = ""
 
 
 
-            for(let product of products){
-                productEl
-            }
+        for (let product of json) {
+            productEl += `
+                <div class="card col-4">
+                    <img src="${product.image}" class="card-img-top img-fluid p-2" style="height: 300px; object-fit: contain;" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title  ">${product.price}</h5>
+                        <p class="card-text">${product.title}</p>
+                        <a href="#" class="btn btn-primary">View More</a>
+                    </div>
+                    </div>
+             `
+        }
+        let productlist = document.getElementById("productInfo")
+        productlist.innerHTML = productEl
+    })
